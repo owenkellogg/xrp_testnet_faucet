53b85b568214b2d40928ace9d18ca468b34bc8bf
Steven Zeiler <stevenzeiler@Stevens-MacBook-Air.local>
2015-10-28 16:29:19 -0700

    [TASK] Enable CORS

--- processes/server.js
+++ processes/server.js
@@ -1,10 +1,20 @@
 var path           = require('path')
 var BridgesExpress = require('bridges-express')
 var port           = process.env.PORT || 5000
+var express        = require('express')
 
 module.exports = function(models) {
 
+  var app = express()
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   var server = new BridgesExpress({
+    app: app,
     directory: path.join(__dirname, '..'),
     controllers: {
       inject: [models]
@@ -15,5 +25,4 @@ module.exports = function(models) {
     console.log('listening on port', port)
   })
 }
-
 
